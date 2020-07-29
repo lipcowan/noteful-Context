376e1fe17d4917b85811a47174a442d534906c77
Lip Cowan <pcowan90@gmail.com>
2020-07-29 13:16:46 -0700

    saving before last step

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -4,35 +4,44 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Note from '../Note/Note'
 import CircleButton from '../CircleButton/CircleButton'
 import './NoteListMain.css'
+import NoteContext from '../NoteContext'
 
-export default function NoteListMain(props) {
-  return (
-    <section className='NoteListMain'>
-      <ul>
-        {props.notes.map(note =>
-          <li key={note.id}>
-            <Note
-              id={note.id}
-              name={note.name}
-              modified={note.modified}
-            />
-          </li>
-        )}
-      </ul>
-      <div className='NoteListMain__button-container'>
-        <CircleButton
-          tag={Link}
-          to='/add-note'
-          type='button'
-          className='NoteListMain__add-note-button'
-        >
-          <FontAwesomeIcon icon='plus' />
-          <br />
-          Note
-        </CircleButton>
-      </div>
-    </section>
-  )
+export default class NoteListMain extends React.Component{
+  static contextType=NoteContext
+
+  render(){
+    let { notes } = this.context;
+    let folderId = this.props.match.params.folderId
+    if (folderId) notes = notes.filter(n => n.folderId === folderId)
+    return (
+      <section className='NoteListMain'>
+        <ul>
+          {notes.map(note =>
+            <li key={note.id}>
+              <Note
+                id={note.id}
+                name={note.name}
+                modified={note.modified}
+              />
+            </li>
+          )}
+        </ul>
+        <div className='NoteListMain__button-container'>
+          <CircleButton
+            tag={Link}
+            to='/add-note'
+            type='button'
+            className='NoteListMain__add-note-button'
+          >
+            <FontAwesomeIcon icon='plus' />
+            <br />
+            Note
+          </CircleButton>
+        </div>
+      </section>
+    )
+  }
+  
 }
 
 NoteListMain.defaultProps = {
