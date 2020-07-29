296ad6ee045061b77f671a6375d2edc6c7d99b35
Ryan Whitmore <ryan@whitmorespace.com>
2020-07-29 12:17:26 -0700

    getting the context to actually work, init empty objects

--- src/NotePageMain/NotePageMain.js
+++ src/NotePageMain/NotePageMain.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import Note from '../Note/Note'
-import './NotePageMain.css'
+/* eslint-disable quotes */
+import React, { Component } from "react";
+import Note from "../Note/Note";
+import "./NotePageMain.css";
 
-export default function NotePageMain(props) {
-  return (
-    <section className='NotePageMain'>
-      <Note
-        id={props.note.id}
-        name={props.note.name}
-        modified={props.note.modified}
-      />
-      <div className='NotePageMain__content'>
-        {props.note.content.split(/\n \r|\n/).map((para, i) =>
-          <p key={i}>{para}</p>
-        )}
-      </div>
-    </section>
-  )
+import NoteContext from "../NoteContext";
+
+export default class NotePageMain extends Component {
+  static contextType = NoteContext;
+  render() {
+    console.log(this.context);
+    let { notes } = this.context;
+    let note = notes.find(n => n.id === this.props.match.params.noteId);
+    const { id = "", name = "", modified = "", content = "" } = note || {};
+    return (
+      <section className="NotePageMain">
+        <Note id={id} name={name} modified={modified} />
+        <div className="NotePageMain__content">
+          {content.split(/\n \r|\n/).map((para, i) => (
+            <p key={i}>{para}</p>
+          ))}
+        </div>
+      </section>
+    );
+  }
 }
 
 NotePageMain.defaultProps = {
   note: {
-    content: '',
+    content: ""
   }
-}
+};
